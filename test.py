ac3fa7e821a3362f266abf3f86bb6453d507df38
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-31 07:01:16 -0700

    tf version + gpus

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-
-if __name__ == '_main':
-    print("TensorFlow version:", tf.__version__)
-    print("Num GPUs Available: ", len(
-        tf.config.experimental.list_physical_devices('GPU')))
-    tf.config.list_physical_devices('GPU')
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available: ", len(
+    tf.config.experimental.list_physical_devices('GPU')))
+tf.config.list_physical_devices('GPU')
