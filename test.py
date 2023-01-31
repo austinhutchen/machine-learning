089b743dfa7d88dd1f11c47c4b3cc75935122b5c
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-31 12:04:54 -0700

    gpu checkl

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 print("TensorFlow version:", tf.__version__)
-
-print("Num CPUs Available: ", len(
-    tf.config.experimental.list_physical_devices('CPU')))
-
+print("Num CPUs Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
