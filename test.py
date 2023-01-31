6510adcfe8073a38e3de55438e18f48bfa6dd415
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-31 12:05:34 -0700

    comments

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+
+
+##Goal of this program is to take in an arbitary dataset and decide on a function to map to output
+
+
 print("TensorFlow version:", tf.__version__)
 print("Num CPUs Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
