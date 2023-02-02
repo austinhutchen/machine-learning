6f77ae50009e471a1ec0cee8bb4f1a4d060b069b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-02-02 08:30:06 -0700

    probability

--- test.py
+++ test.py
@@ -24,6 +24,23 @@ def get_label_indices(labels):
     return label_indices
 
 
+def get_likelihood(features, label_indices, smoothing=0): 
+      #  Compute likelihood based on training samples
+      #  @param features: matrix of features
+        # @param label_indices: grouped sample indices by class
+        # @param smoothing: integer, additive smoothing parameter
+       # @return: dictionary, with class as key, corresponding
+              #   conditional probability P(feature|class) vector
+             #    as value
+ likelihood = {}
+ for label, indices in label_indices.items():
+            likelihood[label] = features[indices, :].sum(axis=0) + smoothing
+            total_count = len(indices)
+            likelihood[label] = likelihood[label] / \
+                (total_count + 2 * smoothing)
+ return likelihood
+
+
 if __name__ == "__main__":
     # training set x ( think of as rows/m in mxn matrix)
     x_train = np.array([
